a10168f98aa9412a82df0a398c4b64251fedadd8
shubhamkhan <shubhamkhanit@gmail.com>
2022-01-26 15:54:09 +0530

    Responsive Header

--- client/src/components/view/Header/Header.js
+++ client/src/components/view/Header/Header.js
@@ -27,13 +27,17 @@ const Header = () => {
     }
 
     return (
-        <nav className="app-bg-primary app-text-primary">
-            <div className="container d-flex flex-wrap">
-                <p className="navbar-brand mt-2">
-                    <img src={logo} alt='Brand Logo' width="32" height="32" className="pt-1 d-inline-block align-text-top"/>
-                    {configDB.APP_NAME}
-                </p>
-                <ul className="nav me-auto mt-1">
+        <nav className="navbar navbar-expand-lg navbar-dark app-bg-primary app-text-primary">
+            <div className="container-fluid">
+            <a className="navbar-brand mb-2" href="#">
+                <img src={logo} alt='Brand Logo' width="32" height="32" className="d-inline-block align-text-top"/>
+                {configDB.APP_NAME}
+            </a>
+            <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="navbar-collapse collapse" id="navbarColor02">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <Link to="/">
                     <li className="nav-item mt-2">
                         <p className="nav-link app-text-primary">Home</p>
@@ -44,17 +48,17 @@ const Header = () => {
                         <p className="nav-link app-text-primary">About</p>
                     </li>
                     </Link>
-                    <li className="nav-item mt-2">
-                        <p className="app-text-primary input-group mb-3">
-                            <input type="text" id="search" className="form-control btn-outline-light" placeholder="Search movie" aria-label="Search movie" aria-describedby="button-addon2" />
-                            <button className="btn btn-light" type="button" id="button-addon2">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
-                                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                                </svg>
-                            </button>
-                        </p>
-                    </li>
                 </ul>
+                <form className="d-flex">
+                    <div className="app-text-primary input-group">
+                        <input type="text" id="search" className="form-control btn-sm btn-outline-light" placeholder="Search movie" aria-label="Search movie" aria-describedby="button-addon2" />
+                        <button className="btn btn-sm btn-light" type="button" id="button-addon2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                            </svg>
+                        </button>
+                    </div>
+                </form>
                 <ul className="nav mt-2">
                     {/* <li className="nav-item mt-2">
                         <p className="app-text-primary">
@@ -83,6 +87,7 @@ const Header = () => {
                         </p>
                     </li>
                 </ul>
+            </div>
             </div>
         </nav>
     )
