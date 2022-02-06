2cbc62e38d225c9a0058d1e9c282722ea512395c
shubhamkhan <shubhamkhanit@gmail.com>
2022-02-06 23:15:01 +0530

    package.json update

--- index.js
+++ index.js
@@ -18,7 +18,16 @@ app.use(express.urlencoded({extended:true}));
 
 
 //Server listening
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 app.listen(port, () => {
     console.log(`Server URL: http://localhost:${port}`)
 });
